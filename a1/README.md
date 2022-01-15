a223ab761ee93c159fada09a1e122c6de895a51b
ZonaHu <hzm5622@126.com>
2022-01-15 01:41:24 -0500

    starting q1-2

--- a1/README.md
+++ a1/README.md
@@ -1,13 +1,15 @@
- # A1 Notes
-   Zuomiao Hu (z228hu 20815304)
+# A1 Notes
+Zuomiao Hu (z228hu 20815304)
  
-    ## Setup
-    * macOS 10.14.6 
-    * IntelliJ IDEA 2021.3 (Community Edition)
-    * kotlin.jvm 1.5.21
-    * Java SDK 16.0.2 (temurin)
- 
-    ## Enhancement 
-    I added a time travel user interface: 
-    * press P to open a portal to another dimension in time
-    * …
+## Setup
+* macOS 12.1
+* IntelliJ IDEA 2021.3.1 (Ultimate Edition)
+* kotlin.jvm 1.5.21
+* Java SDK 16.0.2 (Amazon Corretto)
+* JavaFX 11.0.2
+
+
+## Enhancement 
+I added a time travel user interface: 
+* press P to open a portal to another dimension in time
+* …
--- a1/a1-basic/src/main/kotlin/Main.kt
+++ a1/a1-basic/src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.*
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.HBox
+import javafx.scene.text.Font
+import javafx.stage.Stage
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class Main : Application() {
+    private val layout = BorderPane()
+
+    override fun start(stage: Stage) {
+//
+//        The application opens as a window named “A1 Notes (userid)” with a toolbar at the top and a status bar at the bottom. “Userid” is your UWaterloo user id.
+//        The initial size of the application part of the window is 800 by 600 units when opened.
+//        The layout is “responsive” with a minimum size of 400 by 400 units.
+//
+//        The toolbar has 4 buttons (Add, Random, Delete, Clear), a smaller toggle-button labeled (“!”), and an editable text field for searching.
+
+//         set the title
+        stage.title = "A1 Notes (z228hu)"
+
+//         create the tool bar
+        val toolBar = ToolBar()
+//        val sourcePath = "${System.getProperty("user.dir")}/src/main/resources/"
+
+        val addButn = Button("Add")
+        addButn.font = Font("Helvetica", 14.0)
+        val randomButn = Button("Random")
+        val deleteButn= Button("Delete")
+//        val importantButn = Button()
+
+        val scene = Scene((layout), 800.0, 600.0)
+        toolBar.items.addAll(addButn, randomButn, deleteButn)
+
+        val topPart = HBox()
+        topPart.children.add(toolBar)
+
+
+        layout.top = topPart
+        // show the scene
+        stage.scene = scene
+        stage.show()
+    }
 }
