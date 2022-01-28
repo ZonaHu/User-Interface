ddceb2f3087d374556f04b6b3f1ff490a4cb4b22
Zuomiao Hu <zuomiao.hu@uwaterloo.ca>
2022-01-28 22:54:38 +0000

    Update Main.kt

--- a1/a1-basic/src/main/kotlin/Main.kt
+++ a1/a1-basic/src/main/kotlin/Main.kt
@@ -3,7 +3,6 @@ import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.Scene
 import javafx.scene.control.*
-import javafx.scene.input.MouseButton
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
 import javafx.stage.Stage
