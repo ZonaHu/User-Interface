a81a10e2b6149ec3238273c6c35540168a37ff61
ZonaHu <hzm5622@126.com>
2022-01-28 10:24:10 -0500

    clean up code

--- a1/a1-basic/src/main/kotlin/Main.kt
+++ a1/a1-basic/src/main/kotlin/Main.kt
@@ -12,20 +12,16 @@ class Main : Application() {
     // 全局变量是个数组（所有notes 数据结构 no ui）
     // 刷新 函数 把Flow pane children clear掉再根据数组创建
     // background, margin, spacing
-
     // 弹出框 VBox 是第二个元素在stack Pane
-    private val layout = BorderPane()
 
+    private val layout = BorderPane()
     // create the toolbar
     private val toolBar = HBox()
-
     // bottom status bar
     private val statusBar = HBox()
-
     // scroll pane + flow pane in the center to display notes since notes flow left to right
     private val flowPane = FlowPane()
     private val stackPane = StackPane(layout)
-
     // use scroll pane since we want to show a scroll bar when there are too many notes to fit height-wise
     private val scrollPane = ScrollPane(flowPane)
 
@@ -42,13 +38,11 @@ class Main : Application() {
 
     // counter for the number of notes existed
     private var noteCounter = 0
-
     // an id that denotes the current note's id that is clicked
     private var clickedId = 0
 
     //  an id that denotes the last note's id that is edited
     private var editedId = 0
-
     private var numDisplayed = 0
 
     // displayed number of notes
@@ -57,7 +51,6 @@ class Main : Application() {
 
     // the string denotes current status of the status bar
     private var curStatus = ""
-
     // determine the text that status bar should display
     private var statusText = ""
     private var statusText2 = ""
@@ -123,24 +116,24 @@ class Main : Application() {
         inputBox.textProperty()
             .addListener { _, _, inputText -> refreshDisplay(importantButn.isSelected, inputText.lowercase()) }
 
-        // ============== handle the click actions with the notes =====================================
+        // ============== handle the click actions with the notes ============================================
 
 
-        // ========================================================================================
+        // ===============================set up the default status bar =========================================
         // the text in the status bar is initialized to 0
         statusBar.children.add(Label("0"))
         statusBar.padding = Insets(10.0)
         statusBar.spacing = 30.0
 
-        // set up the layout and the scene
+        // ===================================set up the layout and the scene====================================
         layout.top = toolBar
         layout.center = scrollPane
         layout.bottom = statusBar
 
-        // =========== create the scene with initialized size of 800 by 600 units ===========
+        // =========== create the scene with initialized size of 800 by 600 units ===============================
         val scene = Scene((stackPane), 800.0, 600.0)
 
-        // show the scene
+        // ============================ show the scene===========================================================
         stage.scene = scene
         stage.minWidth = 400.0
         stage.minHeight = 400.0
