c52153ac830432c0621cf47fb16f03c187f7236c
ZonaHu <hzm5622@126.com>
2022-02-14 12:35:30 -0500

    initialized stats view

--- a2/a2-basic/src/main/kotlin/DataStatisticsView.kt
+++ a2/a2-basic/src/main/kotlin/DataStatisticsView.kt
@@ -1,3 +1,5 @@
+import javafx.geometry.Insets
+import javafx.scene.control.Label
 import javafx.scene.control.ScrollPane
 import javafx.scene.layout.FlowPane
 import javafx.scene.layout.GridPane
@@ -6,7 +8,7 @@ import javafx.scene.layout.VBox
 
 class DataStatisticsView (
     private val model: Model
-) : FlowPane(), IView {
+) : GridPane(), IView {
 
     override fun updateView() {
 //        ("Not yet implemented")
@@ -14,9 +16,37 @@ class DataStatisticsView (
 
     init{
         // a 125 unit wide right side area for data statistics
-        width = 125.0
+        minWidth = 125.0
         maxWidth = 125.0
-//        style = " -fx-background-color:black"
+        padding = Insets(5.0)
+        hgap = 5.0
+        vgap = 10.0
+
+//        for(index, data in model.dataset.data.withIndex()){
+//            this.add(Label($index), 0, index, 1, 1)
+//            this.add(Spinner(),1,index,1,1)
+//        }
+
+        //  a row counter
+        var r = 0
+        val numL = Label("Number")
+        add(numL, 1, r)
+        r++
+
+        val minL = Label("Minimum")
+        add(minL, 1, r)
+        r++
+
+        val maxL = Label("Maximum")
+        add(maxL, 1, r)
+        r++
+
+        val avgL = Label("Average")
+        add(avgL, 1, r)
+        r++
+
+        val sumL = Label("Sum")
+        add(sumL, 1, r)
 
         // register with the model when we're ready to start receiving data
         model.addView(this)
