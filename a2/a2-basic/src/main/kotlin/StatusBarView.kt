8db181e8846c7f6d6ac672185d24a523fec08f1d
ZonaHu <hzm5622@126.com>
2022-02-14 01:05:34 -0500

    more initialization

--- a2/a2-basic/src/main/kotlin/StatusBarView.kt
+++ a2/a2-basic/src/main/kotlin/StatusBarView.kt
@@ -9,24 +9,23 @@ class StatusBarView (
 
     override fun updateView() {
         // check if a note is selected
-        count.text = "8"
+        count.text = "6"
         // the message is super easy, just ask the model
-        message.text = "${model.message}"
+        message.text = model.message
     }
 
-    val message = Label("")
-    val count = Label("")
-
+    private val message = Label("")
+    private val count = Label("")
 
     init {
         this.minHeight = 40.0
         // status bar layout
         alignment = Pos.CENTER_LEFT
         padding = Insets(10.0)
-        spacing = 30.0
+        spacing = 5.0
         style = "-fx-background-color:LIGHTGRAY"
 
-//        children.add(count, message)
+        children.addAll(count, message)
 
         // add to the model when we're ready to start receiving data
         model.addView(this)
