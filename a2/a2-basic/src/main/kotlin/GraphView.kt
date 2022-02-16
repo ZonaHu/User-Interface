7727ce929443ec7419ad738b3ecc86e19cbc9eb3
ZonaHu <hzm5622@126.com>
2022-02-16 03:15:23 -0500

    started canvas

--- a2/a2-basic/src/main/kotlin/GraphView.kt
+++ a2/a2-basic/src/main/kotlin/GraphView.kt
@@ -1,8 +1,17 @@
-import javafx.scene.layout.GridPane
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.scene.canvas.Canvas
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.StackPane
+import javafx.scene.paint.Color
 
 class GraphView (
     private val model: Model
-) : GridPane(), IView {
+) : StackPane(), IView {
+
+    private val canvas = Canvas(250.0, 250.0)
+    private val gc = canvas.graphicsContext2D
+
 
     override fun updateView() {
 //        ("Not yet implemented")
@@ -12,9 +21,27 @@ class GraphView (
         // set the background color to white
         style = " -fx-background-color:white"
 
+        // Create a scene graph with a root node that holds the objects that we want to display on the stage
+        val root = Group()
 
+        // Create a canvas as a drawing surface
+        var canvas = Canvas()
 
+        // Use the graphics context to draw on a canvas
+        val gc = canvas.graphicsContext2D
+        gc.lineWidth = 5.0
+        gc.stroke = Color.WHITE
+        gc.fill = Color.WHITE
+        gc.fillRect(75.0, 75.0, 100.0, 100.0)
+        gc.fill = Color.AQUA
+        gc.fillRect(100.0, 100.0, 100.0, 100.0)
+        gc.fill = Color.YELLOWGREEN
+        gc.fillRect(125.0, 125.0, 100.0, 100.0)
 
+        // Add the canvas to the scene
+        root.children.add(canvas)
+
+        children.add(root)
         // register with the model when we're ready to start receiving data
         model.addView(this)
     }
