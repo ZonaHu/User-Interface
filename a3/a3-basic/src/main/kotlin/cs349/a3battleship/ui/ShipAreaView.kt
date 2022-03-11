1ae33baa311a6316645d3fdc4f8c67754a997d7c
ZonaHu <hzm5622@126.com>
2022-03-11 01:00:48 -0500

    initialized middle part

--- a3/a3-basic/src/main/kotlin/cs349/a3battleship/ui/ShipAreaView.kt
+++ a3/a3-basic/src/main/kotlin/cs349/a3battleship/ui/ShipAreaView.kt
@@ -1,24 +1,43 @@
 package cs349.a3battleship.ui
 
 import cs349.a3battleship.model.Game
+import javafx.geometry.Pos
+import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
+import javafx.scene.text.Font
 
 class ShipAreaView(private val model: Game): VBox(), IView {
+    // the player fleet
 
-    private val title = Label("My Formation")
+    private val title = Label("My Fleet")
     private val hbox = HBox(title)
+    private val shipArea = HBox()
+    private val startButn = Button("Start Game")
+    private val exitButn = Button("Exit Game")
 
     override fun updateView() {
+        children.addAll(hbox,shipArea,startButn,exitButn )
+
         println("update ship area")
     }
 
     init{
-        style = "-fx-background-color:YELLOW"
         // 300(board) + 25(board coords) + 25(board coords) = 350 units
         minWidth = 175.0
         maxWidth = 175.0
+
+        hbox.prefHeight = 25.0
+        hbox.alignment = Pos.CENTER
+        title.font = Font("Arial", 16.0)
+        title.style = "-fx-font-weight: bold"
+        title.alignment = Pos.CENTER
+
+        shipArea.prefHeight = 280.0
+        startButn.prefWidth = 175.0
+        startButn.isDisable = true
+        exitButn.prefWidth = 175.0
 
         // add to the model when we're ready to start receiving data
         model.addView(this)
