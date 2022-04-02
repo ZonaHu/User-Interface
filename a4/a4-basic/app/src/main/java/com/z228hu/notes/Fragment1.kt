88a1d8f843d82ea5c3193144a721ca18d3a633b0
ZonaHu <hzm5622@126.com>
2022-04-01 20:25:58 -0400

    update set up

--- a4/a4-basic/app/src/main/java/com/z228hu/notes/Fragment1.kt
+++ a4/a4-basic/app/src/main/java/com/z228hu/notes/Fragment1.kt
@@ -1,59 +1,44 @@
 package com.z228hu.notes
 
 import android.os.Bundle
+import android.view.*
+import android.widget.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Fragment1.newInstance] factory method to
- * create an instance of this fragment.
- */
+// based on the starting template
 class Fragment1 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val viewModel: MyViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment1.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Fragment1().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // layout is defined in "res/layouts/fragment_1.xml"
+        val root = inflater.inflate(R.layout.fragment_1, container, false)
+
+        // add UI handles for navigation here
+        val button1 = root.findViewById<Button>(R.id.button_addNote)
+        button1.setOnClickListener {
+            // (setup navigation actions in "rs/navigation/navigation.xml")
+            findNavController().navigate(R.id.action_f1_to_f2)
+        }
+
+        // add UI handlers that call your viewmodel here
+
+        // observe viewModel properties here
+
+        viewModel.property.observe(this) {
+            println("observe property $it")
+            // update UI here
+
+        }
+
+        return root
     }
 }
---
+++ a4/a4-basic/app/src/main/java/com/z228hu/notes/MyViewModel.kt
@@ -0,0 +1,15 @@
+package com.z228hu.notes
+import androidx.lifecycle.*
+
+// from starter code
+class MyViewModel() : ViewModel() {
+
+    // add all observable properties here
+    val property: MutableLiveData<Int> = MutableLiveData<Int>(0)
+
+    init {
+    }
+
+    // add modelview update functions here
+
+}
